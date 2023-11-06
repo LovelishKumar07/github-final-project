297749d9b78bc665bf716114c2c5af0834403059
LovelishKumar07 <148336280+LovelishKumar07@users.noreply.github.com>
2023-11-06 14:57:15 +0530

    Update Simple-Interest.sh

--- Simple-Interest.sh
+++ Simple-Interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # GoblinSS
+   # LovelishKumar07
    # Input:
    # p, principal amount
    # t, time period in years
